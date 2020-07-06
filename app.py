e018e18836d6e14e414ccd360a419c44225a4f10
ken <ken@ken.local>
2020-07-06 20:26:11 +0800

    updated app.py

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-PORT = 4390
-
-
-@app.route('/')
-def homepage():
-    return "Howdy hacker!!!"
+# PORT = 4390
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=PORT)
+# @app.route('/')
+# def homepage():
+#     return "Howdy hacker!!!"
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=PORT)
+
+
+#!/usr/bin/env python
+# encoding: utf-8
+
+from bottle import run, post
+
+
+@post('/hello')
+def hello():
+    return'Hello World!'
+
+
+if__name__ == '__main__':
+    run(host='0.0.0.0', port=5000)
